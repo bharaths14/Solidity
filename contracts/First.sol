cb819b2183d47bc0747a63aa05c19be7b6da033c
bharath <subusureshbharath@gmail.com>
2023-07-04 16:52:57 +0530

    contrato  solidity token

---
+++ contracts/First.sol
@@ -0,0 +1,13 @@
+//SPDX-license-idenifier:MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract first {
+    uint   public num=2;
+    int public num1= -1;
+    bool public value= true;
+    bytes public name="bharath";
+    string public name2="suresh";
+    address public onwer= 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+}
